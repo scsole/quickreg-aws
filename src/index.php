3fb293d5293d83e30315cdd6e3eb19100e26db94
Sebastian Sole <scsole@protonmail.com>
2020-08-25 18:05:12 +1200

    Added mysql container with a test database

--- src/index.php
+++ src/index.php
@@ -1,1 +1,33 @@
-<?php phpinfo(); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <div class="container">
+      <?php
+        $connection = new PDO('mysql:host=mysql;dbname=test;charset=utf8', 'root', 'root');
+        $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'test'");
+        $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
+
+        if (empty($tables)) {
+            echo '<p class="text-center">Database <code>test</code> contains no tables.</p>';
+        } else {
+            echo '<p class="text-center">Database <code>test</code> contains the following tables:</p>';
+            echo '<ul class="text-center">';
+            foreach ($tables as $table) {
+                echo "<li>{$table}</li>";
+            }
+            echo '</ul>';
+        }
+      ?>
+    </div>
+  </body>
+</html>
