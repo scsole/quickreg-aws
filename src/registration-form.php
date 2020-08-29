bd4a335b62d6f27e41206c8a104e207f5223b83d
Sebastian Sole <scsole@protonmail.com>
2020-08-29 18:37:19 +1200

    Added skeleton php pages

--- src/registration-form.php
+++ src/registration-form.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>QuickREG</title>
+    <title>Register Online</title>
   </head>
   <body>
 
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#">QuickREG</a>
+        <a class="navbar-brand" href="/">QuickREG</a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -23,38 +23,12 @@
 
         <div class="collapse navbar-collapse" id="navbarToggler">
           <div class="navbar-nav">
-            <a class="nav-item nav-link" href="/registration-form.php">Register Online</a>
+            <a class="nav-item nav-link active" href="#">Register Online</a>
             <a class="nav-item nav-link" href="/registration-numbers.php">Registration Numbers</a>
           </div>
         </div>
       </div>
     </nav>
-
-    <div class="container">
-      <?php
-        $db_host   = 'mysql';
-        $db_name   = 'quickreg';
-        $db_user   = 'webuser';
-        $db_passwd = 'insecure_pw';
-
-        $pdo_dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
-
-        $conn   = new PDO($pdo_dsn, $db_user, $db_passwd);
-        $query  = $conn->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'quickreg'");
-        $tables = $query->fetchAll(PDO::FETCH_COLUMN);
-
-        if (empty($tables)) {
-            echo '<p class="text-center">Database <code>quickreg</code> contains no tables.</p>';
-        } else {
-            echo '<p class="text-center">Database <code>quickreg</code> contains the following tables:</p>';
-            echo '<ul class="text-center">';
-            foreach ($tables as $table) {
-                echo "<li>{$table}</li>";
-            }
-            echo '</ul>';
-        }
-      ?>
-    </div>
 
     <!-- JavaScript at the end of page to load last -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
