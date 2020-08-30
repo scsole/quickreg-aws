914b24fdd45a640dc5995d2b8004a2a8d3fa20fd
Josh Mullin <jmull0@protonmail.com>
2020-08-30 15:27:52 +1200

    merge php and html into one file

--- src/index.php
+++ src/index.php
@@ -1,3 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    
+	<body> 
+        <fieldset>
+			<legend><h1>Register</h1></legend>
+			<form action="" method="POST">
+				<label for="first_name">First Name</label>
+				<input type="text" name="first_name" id="first_name" maxlength="255" required>
+
+				<br>
+				<label for="last_name">Last Name</label>
+				<input type="text" name="last_name" id="last_name" maxlength="255" required>
+
+				<br>
+				<label>Gender</label>
+				<input type="radio" id="male" name="gender" value="male" required>
+			  	<label for="male">Male</label>
+			  	<input type="radio" id="female" name="gender" value="female" required>
+			  	<label for="female">Female</label>
+
+ 				<br> 
+				<label for="club">Club</label>
+				<input type="text" name="club" id="club" maxlength="255">
+
+				<br>
+				<label for="dob">Date of Birth</label>
+				<input type="date" name="dob" id="dob" required>
+
+				<br>
+				<label for="email_address">Email Address</label>
+				<input type="email" name="email_address" id="email_address" required maxlength="254">
+
+				<br>
+				<label for="medical_conditions">Medical Conditions</label>
+				<input type="text" name="medical_conditions" id="medical_conditions" maxlength=255">
+
+				<br>
+				<label for="emergency_contact_name">Emergency Contact Name</label>
+				<input type="text" name="emergency_contact_name" id="emergency_contact_name" required maxlength="255">
+
+				<br>
+				<label for="emergency_contact_number">Emergency Contact Number</label>
+				<input type="text" name="emergency_contact_number"  id="emergency_contact_number" required maxlength="40">
+				<br>
+				<button>Register</button>
+            </form>            
+        </fieldset>
+    </body>
+</html>
 <?php
 	function throw_if_null($key, $value){
 		if ($value == null){
@@ -60,5 +115,6 @@
 
 		$stmt->execute();
 
+	echo '<p>Thank you for registering!</p>';
 	}	
 ?>
