c25fab0fd1704a00f4eea06196b247173e67b322
Sebastian Sole <scsole@protonmail.com>
2020-08-31 11:12:59 +1200

    Using more interesting default names

--- .docker/mysql/db-setup.sql
+++ .docker/mysql/db-setup.sql
@@ -15,11 +15,11 @@ CREATE TABLE registrations (
 );
 
 INSERT INTO `registrations` (`first_name`, `last_name`, `gender`, `dob`, `club`, `email_address`, `medical_conditions`, `emergency_contact_name`, `emergency_contact_number`, `registration_timestamp`) VALUES
-('Example1', 'Person', 'female', '1999-09-19', NULL, 'example1@mail.com', NULL, 'EmergencyContact0', '000 111 2222', NOW()),
-('Example2', 'Person', 'male', '1999-09-19', 'Club1', 'example2@mail.com', NULL, 'EmergencyContact0', '000 111 2222', NOW()),
-('Example3', 'Person', 'female', '1999-09-19', NULL, 'example1@mail.com', NULL, 'EmergencyContact1', '000 222 2222', NOW()),
-('Example4', 'Person', 'male', '1999-09-19', NULL, 'example3@mail.com', NULL, 'EmergencyContact2', '+64 00 333 2222', NOW()),
-('Example5', 'Person', 'female', '1999-09-19', 'Club1', 'example4@mail.com', NULL, 'EmergencyContact1', '000 222 2222', NOW()),
-('Example6', 'Person', 'male', '1999-09-19', NULL, 'example5@mail.com', NULL, 'EmergencyContact2', '+64 00 333 2222', NOW()),
-('Example7', 'Person', 'female', '1999-09-19', 'Club2', 'example6@mail.com', NULL, 'EmergencyContact3', '(+64) 021 222 2222', NOW()),
-('Example8', 'Person', 'male', '1999-09-19', 'Club2', 'example7@mail.com', NULL, 'EmergencyContact2', '+64 00 333 2222', NOW());
+('James', 'Smith', 'male', '1978-01-18', NULL, 'js@mail.com', NULL, 'Emergency Contact0', '000 123 4567', NOW()),
+('John', 'Brown', 'male', '1993-08-10', 'Club1', 'jb@mail.com', NULL, 'Emergency Contact1', '000 111 2222', NOW()),
+('Mary', 'Williams', 'female', '1995-04-05', NULL, 'mw@mail.com', NULL, 'Emergency Contact2', '03 222 2222', NOW()),
+('Patricia', 'Jones', 'female', '1999-04-01', NULL, 'pg@mail.com', NULL, 'Emergency Contact3', '+64 00 333 2222', NOW()),
+('Richard', 'Garcia', 'male', '1983-08-03', 'Club1', 'rg@mail.com', NULL, 'Emergency Contact4', '(000) 222 2222', NOW()),
+('Barbara', 'Davis', 'female', '2001-04-06', NULL, 'bd@mail.com', NULL, 'Emergency Contact5', '+64003332999', NOW()),
+('Barbra', 'Shea', 'female', '1996-10-01', 'Club2', 'bs@mail.com', NULL, 'Emergency Contact6', '(+64) 021 222 2222 or 0221188934', NOW()),
+('Linda', 'Smith', 'female', '2006-08-16', 'Club2', 'ls@mail.com', NULL, 'Emergency Contact7', '+64003332222', NOW());
