e6a720e3d910306e6c557ff3b4c3a419a39c693f
Josh Mullin <jmull0@protonmail.com>
2020-09-30 12:27:13 +1300

    register public subnet one

--- init-vpc.sh
+++ init-vpc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-printf "\nVerifying credentials\n"
+
+printf "\nVerifying credentials\n\n"
+
 aws sts get-caller-identity &> /dev/null
 credentialsCorrect=$?
 if [ $credentialsCorrect -ne 0 ]
@@ -11,11 +13,18 @@ then
 		\n\n$credentials_file \n\nor run the command: \n\naws configure\n"
 	exit $credentialsCorrect
 fi
-VPC_CIDR=10.0.0.0/16
-printf "Creating VPC\n"
-VPC_ID=`aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.{VpcId:VpcId}' --output text`
+
+VPC_CIDR="10.0.0.0/16"
 VPC_NAME="vpc-us-east-1-quickreg"
+printf "Creating VPC: $VPC_NAME\n\n"
+VPC_ID=`aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.{VpcId:VpcId}' --output text`
 aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value="$VPC_NAME"
 
 
-# aws ec2 create-subnet 
+SUBNET_NAME_PUBLIC_ONE="subnet-us-east-1-quickreg-public-1"
+printf "Creating subnet: $SUBNET_NAME_PUBLIC_ONE\n\n"
+SUBNET_CIDR_PUBLIC_ONE="10.0.0.0/24"
+SUBNET_ID_PUBLIC_ONE=`aws ec2 create-subnet --cidr-block $SUBNET_CIDR_PUBLIC_ONE \
+	--vpc-id $VPC_ID --query 'Subnet.{SubnetId:SubnetId}' --output text`
+aws ec2 create-tags --resources $SUBNET_ID_PUBLIC_ONE --tags Key=Name,Value="$SUBNET_NAME_PUBLIC_ONE"
+# SUBNET_PUBLIC_TWO_NAME
