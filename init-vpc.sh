cd4db77891f6b1fbd2f08590950c5bd3dc59b0d3
Josh Mullin <jmull0@protonmail.com>
2020-09-30 11:23:18 +1300

    create named vpc

--- init-vpc.sh
+++ init-vpc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+printf "\nVerifying credentials\n"
 aws sts get-caller-identity &> /dev/null
 credentialsCorrect=$?
 if [ $credentialsCorrect -ne 0 ]
@@ -12,7 +12,10 @@ then
 	exit $credentialsCorrect
 fi
 VPC_CIDR=10.0.0.0/16
+printf "Creating VPC\n"
+VPC_ID=`aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.{VpcId:VpcId}' --output text`
+VPC_NAME="vpc-us-east-1-quickreg"
+aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value="$VPC_NAME"
 
-aws ec2 create-vpc --cidr-block $VPC_CIDR --query "Vpc[-1].VpcId" --output text
 
 # aws ec2 create-subnet 
