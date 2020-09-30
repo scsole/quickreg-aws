2e2d774ab367df9b1d1651486f4357a546601a49
Josh Mullin <jmull0@protonmail.com>
2020-09-30 20:16:54 +1300

    create key pair

--- init-vpc.sh
+++ init-vpc.sh
@@ -13,45 +13,48 @@ then
 		\n\n$credentials_file \n\nor run the command: \n\naws configure\n"
 	exit $credentialsCorrect
 fi
-
+AVAILABILITY_ZONE="us-east-1"
 VPC_CIDR="10.0.0.0/16"
-VPC_NAME="vpc-us-east-1-quickreg"
+VPC_NAME="vpc-$AVAILABILITY_ZONE-quickreg"
 printf "Creating VPC: $VPC_NAME\n\n"
 VPC_ID=`aws ec2 create-vpc --cidr-block 10.0.0.0/16 \
 	--query 'Vpc.{VpcId:VpcId}' --output text`
 aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value="$VPC_NAME"
 
-printf "Creating Internet Gateway\n"
+INTERNET_GATEWAY_NAME="internet-gateway-$AVAILABILITY_ZONE-quickreg"
+printf "Creating Internet Gateway: $INTERNET_GATEWAY_NAME\n"
 INTERNET_GATEWAY_ID=`aws ec2 create-internet-gateway \
 	--query 'InternetGateway.{InternetGatewayId:InternetGatewayId}' --output text`
-INTERNET_GATEWAY_NAME="internet-gateway-us-east-1-quickreg"
 aws ec2 create-tags --resources $INTERNET_GATEWAY_ID \
 	--tags Key=Name,Value="$INTERNET_GATEWAY_NAME"
-printf "Attaching Internet Gateway\n\n"
+printf "Attaching Internet Gateway: $INTERNET_GATEWAY_NAME\n\n"
 aws ec2 attach-internet-gateway --vpc-id $VPC_ID \
 	--internet-gateway-id $INTERNET_GATEWAY_ID
 
 printf "Creating Route Table\n"
 ROUTE_TABLE_ID=`aws ec2 create-route-table --vpc-id $VPC_ID \
 	--query 'RouteTable.{RouteTableId:RouteTableId}' --output text`
-ROUTE_TABLE_NAME="route-table-us-east-1-quickreg"
+ROUTE_TABLE_NAME="route-table-$AVAILABILITY_ZONE-quickreg"
 aws ec2 create-tags --resources $ROUTE_TABLE_ID \
 	--tags Key=Name,Value="$ROUTE_TABLE_NAME"
-printf "Creating Route\n\n"
+printf "Creating Route: $ROUTE_TABLE_NAME\n\n"
 ANYWHERE="0.0.0.0/0"
 aws ec2 create-route --route-table-id $ROUTE_TABLE_ID \
 	--destination-cidr-block $ANYWHERE --gateway-id $INTERNET_GATEWAY_ID > /dev/null
 
 
-SUBNET_NAME_PUBLIC_ONE="subnet-us-east-1-quickreg-public-1"
+SUBNET_NAME_PUBLIC_ONE="subnet-$AVAILABILITY_ZONE-quickreg-public-1"
 printf "Creating subnet: $SUBNET_NAME_PUBLIC_ONE\n"
 SUBNET_CIDR_PUBLIC_ONE="10.0.0.0/24"
+# TODO Find method of concatenating strings that is easier to read
+SUBNET_AVAILABILITY_ZONE_PUBLIC_ONE="${AVAILABILITY_ZONE}a"
 SUBNET_ID_PUBLIC_ONE=`aws ec2 create-subnet --cidr-block $SUBNET_CIDR_PUBLIC_ONE \
-	--vpc-id $VPC_ID --query 'Subnet.{SubnetId:SubnetId}' --output text`
+	--vpc-id $VPC_ID --availability-zone $SUBNET_AVAILABILITY_ZONE_PUBLIC_ONE \
+	--query 'Subnet.{SubnetId:SubnetId}' --output text`
 aws ec2 create-tags --resources $SUBNET_ID_PUBLIC_ONE \
 	--tags Key=Name,Value="$SUBNET_NAME_PUBLIC_ONE"
 
-printf "Making subnet: $SUBNET_NAME_PUBLIC_ONE publicly accessible\n"
+printf "Making subnet: $SUBNET_NAME_PUBLIC_ONE publicly accessible\n\n"
 aws ec2 associate-route-table \
 	--route-table-id $ROUTE_TABLE_ID \
 	--subnet-id $SUBNET_ID_PUBLIC_ONE > /dev/null
@@ -61,10 +64,22 @@ aws ec2 modify-subnet-attribute \
 	--map-public-ip-on-launch
 
 
-SUBNET_NAME_PRIVATE_ONE="subnet-us-east-1-quickreg-private-1"
+SUBNET_NAME_PRIVATE_ONE="subnet-$AVAILABILITY_ZONE-quickreg-private-1"
 printf "Creating subnet: $SUBNET_NAME_PRIVATE_ONE\n\n"
 SUBNET_CIDR_PRIVATE_ONE="10.0.1.0/24"
+SUBNET_AVAILABILITY_ZONE_PRIVATE_ONE="${AVAILABILITY_ZONE}a"
 SUBNET_ID_PRIVATE_ONE=`aws ec2 create-subnet --cidr-block $SUBNET_CIDR_PRIVATE_ONE \
-	--vpc-id $VPC_ID --query 'Subnet.{SubnetId:SubnetId}' --output text`
+	--vpc-id $VPC_ID --availability-zone $SUBNET_AVAILABILITY_ZONE_PRIVATE_ONE \
+	--query 'Subnet.{SubnetId:SubnetId}' --output text`
 aws ec2 create-tags --resources $SUBNET_ID_PRIVATE_ONE \
 	--tags Key=Name,Value="$SUBNET_NAME_PRIVATE_ONE"
+# aws ec2 create-key-pair
+
+KEY_PAIR_NAME="key-pair-quickreg.pem"
+KEY_PAIR_PATH="./$KEY_PAIR_NAME"
+printf "Creating key pair: $KEY_PAIR_NAME\n"
+aws ec2 create-key-pair --key-name $KEY_PAIR_NAME --query 'KeyMaterial' --output text > $KEY_PAIR_PATH
+chmod 400 "$KEY_PAIR_PATH"
+printf "Created key pair: $KEY_PAIR_NAME at $KEY_PAIR_PATH\n\n"
+printf "Finished\n\n"
+
